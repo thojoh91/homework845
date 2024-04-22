830bd54c01b442548d86fcd92555b817ad2e16fa
thojoh91 <thojoh91@hotmail.com>
2024-04-22 12:38:14 +0200

    using s3

--- save_json.js
+++ save_json.js
@@ -1,17 +1,15 @@
 const fs = require("fs");
 const path = require("path");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3()
 
-const save = (favNumber) => {
+const save = async (favNumber) => {
   console.log("saving");
-  fs.writeFile(
-    path.join(__dirname, ".", "number.json"),
-    JSON.stringify(favNumber, null, 2),
-    (error) => {
-      if (error) {
-        throw error;
-      }
-    }
-  );
+  await s3.putObject({
+    Body: JSON.stringify(favNumber, null, 2),
+    Bucket: "your_bucket_name", //Change this to your bucket variable name
+    Key: "number.json",
+  }).promise()
 };
 
 module.exports = { save };
